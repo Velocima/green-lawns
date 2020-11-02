949098e748fb100dbf132cb576cd2e3382497495
Velo <hartley.max@outlook.com>
2020-11-02 16:53:33 +0000

    placeholder logo added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,15 @@
 import '../css/app.css';
+import logo from "../images/logo-placeholder.jpg";
+
 
 const App = () => {
   return (
-    <div className="app-container">Hello World!</div>
+    <div className="app-container">
+      <header>
+        <img src={logo} alt="logo placeholder image." height="200px" width="300px"/>
+        
+      </header>
+    </div>
   )
 }
 
