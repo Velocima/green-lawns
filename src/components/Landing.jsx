7030bbcdda85705f5ca7a5c4f6029c012dc13d67
Velo <hartley.max@outlook.com>
2020-11-03 23:03:43 +0000

    basic content added to landing component

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -2,5 +2,24 @@ import React from 'react';
 import '../css/landing.css';
 
 export const Landing = () => {
-	return <div className='landing-container'>Hello Landing!</div>;
+	return (
+		<main className='landing-container'>
+			<div className='banner'>
+				<h1>Green Lawns Boarding Kennels</h1>
+				<h2>Your dogs' home away from home</h2>
+			</div>
+			<p>
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit
+				nisi fugiat quod minus vero quo porro cumque deserunt numquam
+				sit enim id repudiandae amet suscipit corrupti, ipsa
+				consectetur. Voluptas, esse?
+				<br></br>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit.
+				Dignissimos soluta illo quae accusamus labore asperiores!
+				Tenetur libero distinctio quam similique dolor aut, maiores eum
+				at amet officia nisi consequuntur repudiandae?
+			</p>
+			<section class='testimonials'></section>
+		</main>
+	);
 };
