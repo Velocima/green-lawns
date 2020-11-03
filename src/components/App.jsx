9ac2b4e30300dc7f9598d3de4d90a6d264f7faf1
Velo <hartley.max@outlook.com>
2020-11-03 22:13:20 +0000

    logo moved into nav component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import '../css/app.css';
-import logo from '../images/logo-placeholder.jpg';
+import { Nav } from './Nav';
 
 const App = () => {
 	return (
 		<div className='app-container'>
 			<header>
-				<img
-					src={logo}
-					alt='logo placeholder image.'
-					height='200px'
-					width='300px'
-				/>
+				<Nav />
 			</header>
 		</div>
 	);
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import '../css/nav.css';
+import logo from '../images/green-lawns-logo.png';
 
 export const Nav = () => {
-	return <div className='landing-container'>Hello Landing!</div>;
+	return (
+		<nav>
+			<img src={logo} alt='logo placeholder image.' />
+		</nav>
+	);
 };
