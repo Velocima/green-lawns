94cc992249a42e037b7e1629711a8db486463ed7
Velo <hartley.max@outlook.com>
2020-11-03 22:42:58 +0000

    landing imported into app. rendered at root path

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import '../css/app.css';
 import { Nav } from './Nav';
+import { Landing } from './Landing';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 const App = () => {
 	return (
 		<Router>
-			<div className='app-container'>
-				<header>
-					<Nav />
-				</header>
-			</div>
+			<header>
+				<Nav />
+			</header>
+			<Switch>
+				<Route path='/'>
+					<Landing />
+				</Route>
+			</Switch>
 		</Router>
 	);
 };
