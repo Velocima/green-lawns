6f1c0e1ca2da656243c8142e1f8e49da474248b8
Velo <hartley.max@outlook.com>
2020-11-03 22:25:45 +0000

    Nav component renders links to routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import '../css/app.css';
 import { Nav } from './Nav';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 const App = () => {
 	return (
-		<div className='app-container'>
-			<header>
-				<Nav />
-			</header>
-		</div>
+		<Router>
+			<div className='app-container'>
+				<header>
+					<Nav />
+				</header>
+			</div>
+		</Router>
 	);
 };
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import '../css/nav.css';
 import logo from '../images/green-lawns-logo.png';
+import { Link } from 'react-router-dom';
 
-export const Nav = () => {
+export const Nav = (props) => {
+	const pages = [
+		{ route: 'home', navText: 'Home' },
+		{ route: 'about', navText: 'About Us' },
+		{ route: 'services', navText: 'Services' },
+		{ route: 'contact', navText: 'Contact' },
+	];
 	return (
 		<nav>
 			<img src={logo} alt='logo placeholder image.' />
+			<ul>
+				{pages.map((page) => (
+					<li>
+						<Link to={page.route}>{page.navText}</Link>
+					</li>
+				))}
+			</ul>
 		</nav>
 	);
 };
