0537101abb3529adb8bdc5c36ce5d868a1bfe502
Velo <hartley.max@outlook.com>
2020-11-13 17:44:49 +0000

    filler text replaced

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -18,14 +18,19 @@ export default function About() {
 						More than just <br /> a business
 					</h1>
 					<p>
-						This section will contain a paragraph about "family run
-						business". Cover who runs it, why they love it etc.
-						<br></br>
-						Lorem ipsum dolor, sit amet consectetur adipisicing
-						elit. Debitis unde explicabo veritatis fuga labore
-						facere natus nihil, blanditiis consectetur fugiat id
-						accusamus nisi hic eligendi? Omnis, exercitationem. Quo,
-						aperiam perferendis.
+						We are a highly reputable and established boarding
+						kennels offering first class care for our guests. We are
+						situated just outside Monmouth within easy reach of the
+						M4 and A449. Our Kennels are fully licensed by
+						Monmouthshire County Council and Insured by Pet Plan UK
+						Ltd. We pride ourselves in treating each guest as an
+						individual and we care with our kindness and attention
+						to detail. We exercise our doggies three times a day in
+						our beautiful enclosed paddocks where they can socialise
+						with others or, if you prefer can be exercised
+						individually. We specialise in anxious or nervous dogs,
+						giving them the extra time to either make friends with
+						others or to be relaxed just with us.
 					</p>
 				</div>
 			</section>
@@ -35,35 +40,29 @@ export default function About() {
 				</div>
 				<div className='tiles'>
 					<p className='tile-1'>
-						Something about how the kennels are super clean and
-						awesome and dog friendly. <br></br>Lorem ipsum dolor sit
-						amet consectetur adipisicing elit. Doloribus, vel!
-						Quaerat temporibus tempore aut sunt, consequatur
-						suscipit nihil delectus amet quisquam possimus veniam
-						porro perspiciatis similique tempora voluptas sit
-						accusamus?
+						Our Kennels are arranged in two blocks with ten kennels
+						in each. They are all heated and temperature checked
+						twice daily. Every kennel has a inside cosy house and an
+						outside exercise covered area. All bedding and beds are
+						provided but we welcome you to bring yours should you
+						wish.
 					</p>
 					<img src={dog1} alt='' />
-					<img src={dog2} alt='' />
+					<img src={dog3} alt='' />
 					<p className='tile-2'>
-						Somthing about how the good boys get walked lots and
-						love it because every good boy loves a stroll <br></br>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit.
-						Doloribus, vel! Quaerat temporibus tempore aut sunt,
-						consequatur suscipit nihil delectus amet quisquam
-						possimus veniam porro perspiciatis similique tempora
-						voluptas sit accusamus?
+						Our top of the range foods include both wet and dry. If
+						your dog requires a specific diet, we ask that you
+						provide this for them. Bowls are supplied and we offer
+						fresh water in their kennel at all times.
 					</p>
 					<p className='tile-3'>
-						Something about the micheline starred doggo dinners
-						<br></br>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit.
-						Doloribus, vel! Quaerat temporibus tempore aut sunt,
-						consequatur suscipit nihil delectus amet quisquam
-						possimus veniam porro perspiciatis similique tempora
-						voluptas sit accusamus?
+						If your doggie requires medication during their stay, we
+						can happily administer this. We simply require that you
+						write all the details on the medication boxes and we
+						will require you to fill out a quick medical form for
+						them.
 					</p>
-					<img src={dog3} alt='' />
+					<img src={dog2} alt='' />
 				</div>
 			</section>
 			<section className='links'>
