8cd6dbebcd1fd916378d8df6ecea206fdf7b39b8
Velo <hartley.max@outlook.com>
2020-11-15 19:43:39 +0000

    logo animation added

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,13 +7,15 @@ import { pages } from '../contentJs/pages';
 export default function Nav(props) {
 	return (
 		<nav>
-			<div className='logo'>
-				<Link to='/'>
-					<img src={logo} alt='logo placeholder.' />
-				</Link>
-				<h4>
-					Green Lawns <br></br>Boarding Kennels
-				</h4>
+			<div className='overflow-container'>
+				<div className='logo'>
+					<Link to='/'>
+						<img src={logo} alt='logo placeholder.' />
+					</Link>
+					<h4>
+						Green Lawns <br></br>Boarding Kennels
+					</h4>
+				</div>
 			</div>
 			<ul>
 				{pages.map((page, i) => (
