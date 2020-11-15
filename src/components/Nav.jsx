a9dd06f48c1135e70dce72b3dfb131b827d443a4
Velo <hartley.max@outlook.com>
2020-11-15 16:53:50 +0000

    pages array transfered to own file from nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,14 +2,9 @@ import React from 'react';
 import '../css/nav.css';
 import logo from '../images/green-lawns-logo.png';
 import { Link } from 'react-router-dom';
+import { pages } from '../contentJs/pages';
 
 export default function Nav(props) {
-	const pages = [
-		{ route: '/', navText: 'Home' },
-		{ route: '/about', navText: 'About Us' },
-		{ route: '/services', navText: 'Services' },
-		{ route: '/contact', navText: 'Contact' },
-	];
 	return (
 		<nav>
 			<div className='logo'>
---
+++ src/contentJs/pages.js
@@ -0,0 +1,6 @@
+export const pages = [
+    { route: '/', navText: 'Home' },
+    { route: '/about', navText: 'About Us' },
+    { route: '/services', navText: 'Services' },
+    { route: '/contact', navText: 'Contact' },
+];
