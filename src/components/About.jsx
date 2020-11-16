5861377c5da51602e3f93d68fc81f72428da3ff7
Velo <hartley.max@outlook.com>
2020-11-16 02:39:42 +0000

    animation state management added to about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useState, useLayoutEffect } from 'react';
 import '../css/about.css';
 import ownerImage from '../images/good-boys-and-owner.jpg';
 import dog1 from '../images/dog-placeholder-1.jpg';
@@ -6,6 +6,49 @@ import dog2 from '../images/dog-placeholder-2.jpg';
 import dog3 from '../images/dog-placeholder-3.jpg';
 
 export default function About() {
+	const [animationIsPlaying, setAnimationIsPlaying] = useState({
+		facilitiesBanner: 'paused',
+		firstRow: 'paused',
+		secondRow: 'paused',
+		thirdRow: 'paused',
+	});
+
+	const facilitiesBannerRef = useRef(null),
+		firstRowRef = useRef(null),
+		secondRowRef = useRef(null),
+		thirdRowRef = useRef(null);
+
+	useLayoutEffect(() => {
+		const topPosition = (element) => element.current.getBoundingClientRect().top;
+		const bottomPosition = (element) => element.current.getBoundingClientRect().bottom;
+
+		const facilitiesBannerPosition = topPosition(facilitiesBannerRef),
+			firstRowPosition = bottomPosition(firstRowRef),
+			secondRowPosition = bottomPosition(secondRowRef),
+			thirdRowPosition = bottomPosition(thirdRowRef);
+
+		const onScroll = () => {
+			const scrollPosition = window.scrollY + window.innerHeight;
+			if (!animationIsPlaying.facilitiesBanner && facilitiesBannerPosition < scrollPosition) {
+				setAnimationIsPlaying((prevState) => ({ ...prevState, reviewBanner: 'running' }));
+			}
+			if (!animationIsPlaying.firstRow && firstRowPosition < scrollPosition) {
+				setAnimationIsPlaying((prevState) => ({
+					...prevState,
+					reviewBackground: 'running',
+				}));
+			}
+			if (!animationIsPlaying.secondRow && secondRowPosition < scrollPosition) {
+				setAnimationIsPlaying((prevState) => ({ ...prevState, review1: 'running' }));
+			}
+			if (!animationIsPlaying.thirdRow && thirdRowPosition < scrollPosition) {
+				setAnimationIsPlaying((prevState) => ({ ...prevState, review2: 'running' }));
+			}
+		};
+		window.addEventListener('scroll', onScroll);
+		return () => window.removeEventListener('scroll', onScroll);
+	}, [animationIsPlaying]);
+
 	return (
 		<main className='about-container'>
 			<section className='family'>
@@ -35,30 +78,58 @@ export default function About() {
 				</div>
 			</section>
 			<section className='facilities'>
-				<div className='banner'>
+				<div
+					className='banner'
+					ref={facilitiesBannerRef}
+					style={{ animationPlayState: animationIsPlaying.facilitiesBanner }}
+				>
 					<h1>Our facilities</h1>
 				</div>
 				<div className='tiles'>
-					<p className='tile-1'>
+					<p
+						className='tile-1'
+						style={{ animationPlayState: animationIsPlaying.firstRow }}
+					>
 						Our Kennels are arranged in two blocks with ten kennels in each. They are
 						all heated and temperature checked twice daily. Every kennel has a inside
 						cosy house and an outside exercise covered area. All bedding and beds are
 						provided but we welcome you to bring yours should you wish.
 					</p>
-					<img src={dog1} alt='' />
-					<img src={dog3} alt='' />
-					<p className='tile-2'>
+					<img
+						src={dog1}
+						alt=''
+						ref={firstRowRef}
+						style={{ animationPlayState: animationIsPlaying.firstRow }}
+					/>
+					<img
+						src={dog3}
+						alt=''
+						ref={secondRowRef}
+						style={{ animationPlayState: animationIsPlaying.secondRow }}
+					/>
+					<p
+						className='tile-2'
+						style={{ animationPlayState: animationIsPlaying.secondRow }}
+					>
 						Our top of the range foods include both wet and dry. If your dog requires a
 						specific diet, we ask that you provide this for them. Bowls are supplied and
 						we offer fresh water in their kennel at all times.
 					</p>
-					<p className='tile-3'>
+					<p
+						className='tile-3'
+						style={{ animationPlayState: animationIsPlaying.thirdRow }}
+					>
 						If your doggie requires medication during their stay, we can happily
 						administer this. We simply require that you write all the details on the
 						medication boxes and we will require you to fill out a quick medical form
 						for them.
 					</p>
-					<img src={dog2} alt='' />
+					<img
+						src={dog2}
+						alt=''
+						ref={thirdRowRef}
+						style={{ animationPlayState: animationIsPlaying.thirdRow }}
+					/>
 				</div>
 			</section>
 		</main>
