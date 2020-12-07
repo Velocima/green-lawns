e7e79699de3d19f2eecf9d7128b58428c2658401
Velo <hartley.max@outlook.com>
2020-12-07 13:18:26 +0000

    images imported into about, passed as prop to gallery component and rendered

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
 export default function Gallery(props) {
-	return <section className='gallery'>Gallery</section>;
+	return (
+		<section className='gallery'>
+			{props.images.map((image, i) => (
+				<img src={image} alt='' key={i} />
+			))}
+		</section>
+	);
 }
