1a2726f9320dca9c166e351582d14717793d13dd
Velo <hartley.max@outlook.com>
2020-12-07 13:25:22 +0000

    images cropped and rendered inside overflow containers. hover effects to come...

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import '../css/gallery.css';
 
 export default function Gallery(props) {
 	return (
 		<section className='gallery'>
 			{props.images.map((image, i) => (
-				<img src={image} alt='' key={i} />
+				<div className='overflow-container'>
+					<img src={image} alt='' key={i} />
+				</div>
 			))}
 		</section>
 	);
