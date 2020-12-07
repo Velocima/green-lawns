8c1a63a61894a2114fa8eabafd90221fbe0d7233
Velo <hartley.max@outlook.com>
2020-12-07 14:32:50 +0000

    gallery image modal added. opens image to fullscreen on click

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../css/gallery.css';
 
 export default function Gallery(props) {
+	const [isModalOpen, setIsModalOpen] = useState(false);
+	const [modalImage, setModalImage] = useState(props.images[0]);
+
+	const handleImageClick = ({ target }) => {
+		setIsModalOpen(true);
+		setModalImage(target.src);
+	};
+	const handleButtonClick = () => {
+		setIsModalOpen(false);
+	};
+
 	return (
 		<section className='gallery'>
 			{props.images.map((image, i) => (
 				<div className='overflow-container' key={i}>
-					<img src={image} alt='' />
+					<img src={image} alt='' onClick={handleImageClick} />
 				</div>
 			))}
+			<div
+				className='modal'
+				style={{
+					opacity: isModalOpen ? '1' : '0',
+					pointerEvents: isModalOpen ? 'all' : 'none',
+				}}
+			>
+				<button onClick={handleButtonClick}>X</button>
+				<img src={modalImage} alt='' />
+			</div>
 		</section>
 	);
 }
