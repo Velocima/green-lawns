7d0450207d2ddaae7aa4ecf01166591e8b20d30f
Velo <hartley.max@outlook.com>
2020-12-09 14:26:26 +0000

    two new images added to the gallery

--- src/contentJs/galleryImages.js
+++ src/contentJs/galleryImages.js
@@ -7,10 +7,12 @@ import image6 from '../images/gallery/hope-and-ted.jpg';
 import image7 from '../images/gallery/husky.jpg';
 import image8 from '../images/gallery/nani.jpg';
 import image9 from '../images/gallery/pair-of-good-boys.jpg';
+import image10 from '../images/gallery/he-sits.jpg';
 import image11 from '../images/gallery/stick.jpg';
 import image12 from '../images/gallery/ted-1.jpg';
 import image13 from '../images/gallery/thor.jpg';
 import image14 from '../images/gallery/trio.jpg';
+import image15 from '../images/gallery/barley.jpg';
 
 export const galleryImages = [
 	{ image: image1, alt: '' },
@@ -22,8 +24,10 @@ export const galleryImages = [
 	{ image: image7, alt: '' },
 	{ image: image8, alt: '' },
 	{ image: image9, alt: '' },
+	{ image: image10, alt: '' },
 	{ image: image11, alt: '' },
 	{ image: image12, alt: '' },
 	{ image: image13, alt: '' },
 	{ image: image14, alt: '' },
+	{ image: image15, alt: '' },
 ];
