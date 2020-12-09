41215eeebb01d176e15772db99853458ebe67be9
Velo <hartley.max@outlook.com>
2020-12-09 14:15:59 +0000

    image removed as per client request

--- src/contentJs/galleryImages.js
+++ src/contentJs/galleryImages.js
@@ -7,7 +7,6 @@ import image6 from '../images/gallery/hope-and-ted.jpg';
 import image7 from '../images/gallery/husky.jpg';
 import image8 from '../images/gallery/nani.jpg';
 import image9 from '../images/gallery/pair-of-good-boys.jpg';
-import image10 from '../images/gallery/puppy.jpg';
 import image11 from '../images/gallery/stick.jpg';
 import image12 from '../images/gallery/ted-1.jpg';
 import image13 from '../images/gallery/thor.jpg';
@@ -23,7 +22,6 @@ export const galleryImages = [
 	{ image: image7, alt: '' },
 	{ image: image8, alt: '' },
 	{ image: image9, alt: '' },
-	{ image: image10, alt: '' },
 	{ image: image11, alt: '' },
 	{ image: image12, alt: '' },
 	{ image: image13, alt: '' },
