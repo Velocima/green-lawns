ad6a0866161756088fc394f33995826a9bd87034
Velo <hartley.max@outlook.com>
2020-12-09 13:51:11 +0000

    gallery image importing handled in new seperate js file

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,41 +6,7 @@ import ownerImage from '../images/good-boys-and-owner.jpg';
 import dog1 from '../images/dog-placeholder-1.jpg';
 import dog2 from '../images/dog-placeholder-2.jpg';
 import dog3 from '../images/dog-placeholder-3.jpg';
-
-// gallery images
-import image1 from '../images/gallery/runnig.jpg';
-import image2 from '../images/gallery/also-running.jpg';
-import image3 from '../images/gallery/good-boy-ball.jpg';
-import image4 from '../images/gallery/good-boy-three.jpg';
-import image5 from '../images/gallery/good-boy-two.jpg';
-import image6 from '../images/gallery/hope-and-ted.jpg';
-import image7 from '../images/gallery/husky.jpg';
-import image8 from '../images/gallery/nani.jpg';
-import image9 from '../images/gallery/pair-of-good-boys.jpg';
-import image10 from '../images/gallery/puppy.jpg';
-import image11 from '../images/gallery/stick.jpg';
-import image12 from '../images/gallery/ted-1.jpg';
-import image13 from '../images/gallery/thor.jpg';
-import image14 from '../images/gallery/trio.jpg';
-import image15 from '../images/gallery/runnig.jpg';
-
-const galleryImages = [
-	image1,
-	image2,
-	image3,
-	image4,
-	image5,
-	image6,
-	image7,
-	image8,
-	image9,
-	image10,
-	image11,
-	image12,
-	image13,
-	image14,
-	image15,
-];
+import { galleryImages } from '../contentJs/galleryImages';
 
 export default function About() {
 	const [width, height] = useWindowSize();
---
+++ src/contentJs/galleryImages.js
@@ -0,0 +1,33 @@
+import image1 from '../images/gallery/runnig.jpg';
+import image2 from '../images/gallery/also-running.jpg';
+import image3 from '../images/gallery/good-boy-ball.jpg';
+import image4 from '../images/gallery/good-boy-three.jpg';
+import image5 from '../images/gallery/good-boy-two.jpg';
+import image6 from '../images/gallery/hope-and-ted.jpg';
+import image7 from '../images/gallery/husky.jpg';
+import image8 from '../images/gallery/nani.jpg';
+import image9 from '../images/gallery/pair-of-good-boys.jpg';
+import image10 from '../images/gallery/puppy.jpg';
+import image11 from '../images/gallery/stick.jpg';
+import image12 from '../images/gallery/ted-1.jpg';
+import image13 from '../images/gallery/thor.jpg';
+import image14 from '../images/gallery/trio.jpg';
+import image15 from '../images/gallery/runnig.jpg';
+
+export const galleryImages = [
+	image1,
+	image2,
+	image3,
+	image4,
+	image5,
+	image6,
+	image7,
+	image8,
+	image9,
+	image10,
+	image11,
+	image12,
+	image13,
+	image14,
+	image15,
+];
